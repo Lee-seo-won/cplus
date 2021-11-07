056d6a7006a52e16596c29f78569ea49737288e7
Lee-seo-won <qkrpsflek@naver.com>
2021-11-07 12:49:45 +0900

    day01

---
+++ day01/day01/myinfo.cpp
@@ -0,0 +1,15 @@
+/*#include <iostream>
+using namespace std;
+
+
+int main() {
+
+
+	
+	cout << "이름 : 홍길동" << endl;
+	cout << "나이 : 30\n키 : 173.5\n전화번호 : 010-1234-5678\n";
+	
+
+	return 0;
+
+}*/
---
+++ day01/day01/var.cpp
@@ -0,0 +1,19 @@
+/*#include <iostream>
+using namespace std;
+
+
+int main() {
+	char name[10] = "이산";  //'\0'이 존재
+	int grade;  //학년
+	//int class;  반 예약어(키워드) 사용 불가
+	int schoolClass; //반
+
+	grade = 2;
+	schoolClass = 5;
+
+	cout << name << " 학생은 " << grade << "학년 " << schoolClass << "반입니다." << endl;
+
+
+	return 0;
+
+}*/
